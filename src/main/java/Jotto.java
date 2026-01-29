b7b12dbf24dc50e937861832abc563441cafa734
bob234-ui <emiliano.gomez2034@gmail.com>
2026-01-29 11:16:37 -0800

    Add the constructor and methods to Jotto.java

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -1,7 +1,72 @@
 /**
- * @author feng3302
+ * @author Emiliano Gomez
  * @version 0.1.0
  * @Since 1/29/26
  **/
 public class Jotto {
+
+    // Fields
+    private int WORD_SIZE;
+    private String currentWord;
+    private int score;
+    private ArrayList<String> playGuesses;
+    private ArrayList<String> playWords;
+    private String filename;
+    private ArrayList<String> wordList;
+    private boolean DEBUG;
+
+    public Jotto(String filename) {
+    }
+
+    public boolean pickWord() {
+        return false;
+    }
+
+    public String showWordList() {
+        return null;
+    }
+
+    public ArrayList<String> showPlayerGuesses() {
+        return null;
+    }
+
+    void playerGuessScores(ArrayList<String> guesses) {
+    }
+
+    public void setCurrentWord(String word) {
+    }
+
+    public ArrayList<String> readWords() {
+        return null;
+    }
+
+    public void play() {
+    }
+
+    int guess() {
+        return 0;
+    }
+
+    public ArrayList<String> getPlayedWords() {
+        return null;
+    }
+
+    public String getCurrentWord() {
+        return null;
+    }
+
+    public int getLetterCount(String word) {
+        return 0;
+    }
+
+    public String showPlayedWords() {
+        return null;
+    }
+
+    public boolean addPlayerGuess(String guess) {
+        return false;
+    }
+
+    private void updateWordList() {
+    }
 }
